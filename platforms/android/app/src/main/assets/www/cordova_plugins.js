801578d98e9913b382cdc52d88d74f33489069e0
Your Name <e@w.com>
2023-02-13 18:40:36 +0300

    testingMessageApi

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -9,9 +9,9 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-barcodescanner.BarcodeScanner",
-      "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
-      "pluginId": "cordova-plugin-barcodescanner",
+      "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+      "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+      "pluginId": "phonegap-plugin-barcodescanner",
       "clobbers": [
         "cordova.plugins.barcodeScanner"
       ]
@@ -19,6 +19,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   ];
   module.exports.metadata = {
     "cordova-plugin-inappbrowser": "5.0.0",
-    "cordova-plugin-barcodescanner": "0.7.4"
+    "phonegap-plugin-barcodescanner": "8.1.0"
   };
 });
--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -22,31 +22,106 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
  function  onDeviceReady()  {
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-    document.getElementById('browser').addEventListener('click', () => {
-        console.log('click');
-        console.log(cordova);
-        cordova.InAppBrowser.open('https://cordova.vercel.app/', '_blank')
+    let inAppBrowserRef;
+    function openInAppBrowser() {
+        /* Open URL */
+        let open_url = 'https://cordova.vercel.app';
+        inAppBrowserRef = cordova.InAppBrowser.open(open_url, '_blank', 'clearcache=yes,clearsessioncache=yes,location=yes,hardwareback=no,zoom=no');
+        /* Add event listener to close the InAppBrowser */
+        inAppBrowserRef.addEventListener('message', messageCallBack);
+      }
 
-    })
+      function messageCallBack(params) {
+        /* Close the InAppBrowser if we received the proper message */
+        if (params.data.action == 'scan') {
+            scanBarcode()
+        }
+      }
+      function postCordovaMessage() {
+        /* Send an action = 'close' JSON object to Cordova via postMessage API */
+        var message = {action: 'scan'};
+        if (!webkit.messageHandlers.cordova_iab) {
+          console.warn('Cordova IAB postMessage API not found!');
+          throw 'Cordova IAB postMessage API not found!';
+        } else {
+          console.log('Message sent!');
+          webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(message));
+        }
+      }
 
-     document.getElementById('scan').addEventListener('click',  () => {
-        console.log('click');
+
+
+      function scanBarcode() {
         console.log(cordova);
+        console.log('click');
         cordova.plugins.barcodeScanner.scan(
             function (result) {
                 alert("We got a barcode\n" +
-                      "Result: " + result.text + "\n" +
-                      "Format: " + result.format + "\n" +
-                      "Cancelled: " + result.cancelled);
+                        "Result: " + result.text + "\n" +
+                        "Format: " + result.format + "\n" +
+                        "Cancelled: " + result.cancelled);
             },
             function (error) {
-                alert("Scanning failed: " + error); 
+                alert("Scanning failed: " + error);
+            },
+            {
+                preferFrontCamera : true, // iOS and Android
+                showFlipCameraButton : true, // iOS and Android
+                showTorchButton : true, // iOS and Android
+                torchOn: true, // Android, launch with the torch switched on (if available)
+                saveHistory: true, // Android, save scan history (default false)
+                prompt : "Place a barcode inside the scan area", // Android
+                resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+                disableAnimations : true, // iOS
+                disableSuccessBeep: false // iOS and Android
             }
-         );
+            );
+      }
+
+    // window.open = cordova.InAppBrowser.open('https://cordova.vercel.app', '_blank', 'location=no');
+    document.getElementById('browser').addEventListener('click', openInAppBrowser)
+    document.getElementById('scan').addEventListener('click',postCordovaMessage)
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    // document.getElementById('deviceready').classList.add('ready');
+    // document.getElementById('browser').addEventListener('click', () => {
+    //     console.log(cordova);
+    //     cordova.InAppBrowser.open('https://addeo.ru/test.html', '_blank')
+
+    // })
+
+ 
+    //  document.getElementById('scan').addEventListener('click',  () => {
+    //     console.log('click');
+    //     console.log(cordova);
+
+    //     cordova.plugins.barcodeScanner.scan(
+    //         function (result) {
+    //             alert("We got a barcode\n" +
+    //                   "Result: " + result.text + "\n" +
+    //                   "Format: " + result.format + "\n" +
+    //                   "Cancelled: " + result.cancelled);
+    //         },
+    //         function (error) {
+    //             alert("Scanning failed: " + error);
+    //         },
+    //         {
+    //             preferFrontCamera : true, // iOS and Android
+    //             showFlipCameraButton : true, // iOS and Android
+    //             showTorchButton : true, // iOS and Android
+    //             torchOn: true, // Android, launch with the torch switched on (if available)
+    //             saveHistory: true, // Android, save scan history (default false)
+    //             prompt : "Place a barcode inside the scan area", // Android
+    //             resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+    //             formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+    //             orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+    //             disableAnimations : true, // iOS
+    //             disableSuccessBeep: false // iOS and Android
+    //         }
+    //      );
  
 
-    })
+    // })
 }
 
