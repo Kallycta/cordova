ddd61aa094797872fa708422db45f3537bd05431
Your Name <e@w.com>
2023-02-14 13:44:47 +0300

    testingMessageApi

--- platforms/android/app/src/main/assets/www/cordova.js
+++ platforms/android/app/src/main/assets/www/cordova.js
@@ -363,13 +363,15 @@ define("cordova/android/promptbasednativeapi", function(require, exports, module
 
 module.exports = {
     exec: function (bridgeSecret, service, action, callbackId, argsJson) {
-        return prompt(argsJson, 'gap:' + JSON.stringify([bridgeSecret, service, action, callbackId]));
+        return 
+        // prompt(argsJson, 'gap:' + JSON.stringify([bridgeSecret, service, action, callbackId]));
     },
     setNativeToJsBridgeMode: function (bridgeSecret, value) {
-        prompt(value, 'gap_bridge_mode:' + bridgeSecret);
+        // prompt(value, 'gap_bridge_mode:' + bridgeSecret);
     },
     retrieveJsMessages: function (bridgeSecret, fromOnlineEvent) {
-        return prompt(+fromOnlineEvent, 'gap_poll:' + bridgeSecret);
+        return
+        //  prompt(+fromOnlineEvent, 'gap_poll:' + bridgeSecret);
     }
 };
 
