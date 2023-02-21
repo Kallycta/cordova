186fede053778bcd7c2235554271dba13b6bec6d
Kallycta <83584576+Kallycta@users.noreply.github.com>
2023-02-21 21:12:34 +0400

    changeColorAndIcon

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -23,11 +23,20 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "navigator.splashscreen"
       ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-inappbrowser": "5.0.0",
     "phonegap-plugin-barcodescanner": "8.1.0",
-    "cordova-plugin-splashscreen": "6.0.2"
+    "cordova-plugin-splashscreen": "6.0.2",
+    "cordova-plugin-statusbar": "3.0.0"
   };
 });
